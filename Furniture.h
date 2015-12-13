7e3a134a4e03e4abc7cbbfd9c8cabddfc29347b8
jappaa <dhwissem@gmail.com>
2015-12-13 11:51:49 -0600

    Create Furniture.h

---
+++ Furniture.h
@@ -0,0 +1,23 @@
+
+using namespace std;
+class Furniture {
+
+private:
+ string name;
+ float size_2D;
+ float m_BoundingBox;
+ float m_Cosmetic;
+ float m_Usage_Space;
+ float weight;
+ int connector;
+
+public:
+
+ void set_name(string);
+ string get_name();
+
+ void set_size_2D(float);
+ float get_size_2D();
+
+
+};
